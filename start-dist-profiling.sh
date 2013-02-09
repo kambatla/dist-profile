09b24ee1fd4da9e1fcf0613270e8d9995df32438
Karthik Kambatla <kasha@cloudera.com>
2013-02-09 04:02:59 -0800

    Copy only *-profiling.sh to the slaves

--- start-dist-profiling.sh
+++ start-dist-profiling.sh
@@ -12,7 +12,7 @@ curr_dir=`pwd`
 target_dir=$curr_dir/$1-stats
 
 mkdir ~/tmp-dist-profiling 
-cp -r * ~/tmp-dist-profiling/
+cp -r *-profiling.sh ~/tmp-dist-profiling/
 
 ./multitool.sh -s "rsync -avz $MASTER:~/tmp-dist-profiling/ ~/tmp-dist-profiling"
 ./multitool.sh -s "cd ~/tmp-dist-profiling; sh start-profiling.sh $1"
